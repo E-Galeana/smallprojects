e32bee78155e4dc8e422981fc537470c40b3cb7a
E-Galeana <emanuelgaleana147@gmail.com>
2025-01-01 23:14:54 -0500

    Improved code readability and consistency by fixing indentation. Implemented detection of Page Up and Page Down keypresses using escape sequences to enhance low-level terminal input handling.

--- kilo.c
+++ kilo.c
@@ -19,7 +19,9 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** data ***/
@@ -82,12 +84,22 @@ int editorReadKey() //waits for keypress and returns it.
       if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
       if (seq[0] == '[') {
-	switch (seq[1]) {
-	  case 'A': return 'ARROW_UP';
-	  case 'B': return 'ARROW_DOWN';
-	  case 'C': return 'ARROW_RIGHT';
-	  case 'D': return 'ARROW_LEFT';
-	}
+	if (seq[1] >= '0' && seq[1] <= '9') {
+	  if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+	  if (seq[2] == '~') {
+	    switch (seq[1]) {
+	      case '5': return PAGE_UP;
+ 	      case '6': return PAGE_DOWN;
+	    }
+	  }
+	} else {
+       	  switch (seq[1]) {
+	    case 'A': return 'ARROW_UP';
+	    case 'B': return 'ARROW_DOWN';
+	    case 'C': return 'ARROW_RIGHT';
+	    case 'D': return 'ARROW_LEFT';
+	  }
+	 }
       }
 
       return '\x1b';
