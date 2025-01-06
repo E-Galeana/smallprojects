aee917ffd81ff74f5398ca70a3c474c4278e42dc
E-Galeana <emanuelgaleana147@gmail.com>
2025-01-05 19:35:37 -0500

    Add detailed comments to all functions for clarity, including explanations of parameters, return values, and functionality

--- linkedlist.c
+++ linkedlist.c
@@ -9,6 +9,22 @@ typedef struct ListNode
     struct ListNode *next;
 } ListNode;
 
+/*
+ * Function: createNode
+ * --------------------
+ * Creates a new node with the given data value.
+ * 
+ * Parameters:
+ *   data - The integer value to store in the new node
+ * 
+ * Returns:
+ *   Pointer to the newly created node.
+ * 
+ * Notes:
+ * - Allocates memory dynamically using malloc.
+ * - Initializes the next pointer to NULL.
+ * - Exits the program if memory allocation fails.
+ */
 ListNode* createNode(int data) {
     ListNode* newNode = malloc(sizeof(*newNode));
     if (newNode == NULL) {
@@ -21,6 +37,22 @@ ListNode* createNode(int data) {
     return newNode;
 }
 
+/*
+ * Function: addNode
+ * -----------------
+ * Adds a new node with the given value at the end of the linked list.
+ * 
+ * Parameters:
+ *   head  - Pointer to the head of the list (can be NULL for an empty list)
+ *   value - The integer value to be added in the new node
+ * 
+ * Returns:
+ *   Pointer to the head of the list after adding the new node.
+ * 
+ * Notes:
+ * - If the list is empty (head is NULL), a new node becomes the head.
+ * - Traverses the list to find the last node, then appends the new node.
+ */
 ListNode* addNode(ListNode *head, int value) {
     ListNode* newNode = createNode(value);
     if (head == NULL) {
@@ -38,6 +70,19 @@ ListNode* addNode(ListNode *head, int value) {
     return head;
 }
 
+/*
+ * Function: printList
+ * -------------------
+ * Prints the entire linked list from head to tail.
+ * 
+ * Parameters:
+ *   node - Pointer to the head of the list
+ * 
+ * Notes:
+ * - Traverses the list and prints each node's data.
+ * - Appends " -> " between nodes for readability.
+ * - Ends the list with "NULL" to indicate the end.
+ */
 void printList(ListNode* node) {
     while (node != NULL) {
         printf("%d", node->data);
@@ -50,6 +95,21 @@ void printList(ListNode* node) {
     printf(" -> NULL\n");
 }
 
+/*
+ * Function: nodeInput
+ * -------------------
+ * Reads and validates user input for the linked list.
+ * 
+ * Returns:
+ *   Integer value entered by the user, or -1 if the user types "exit".
+ * 
+ * Notes:
+ * - Prompts the user to enter a node value or type "exit" to quit.
+ * - Removes the newline character from the input buffer.
+ * - If the user enters "exit", the function returns -1 to indicate termination.
+ * - If the input is a valid integer, it returns the parsed integer value.
+ * - Continues prompting the user in case of invalid input.
+ */
 int nodeInput() {
     char buffer[100];
     int value;
@@ -75,6 +135,18 @@ int nodeInput() {
     }
 }
 
+/*
+ * Function: freeList
+ * ------------------
+ * Frees all nodes in the linked list to release allocated memory.
+ * 
+ * Parameters:
+ *   head - Pointer to the head of the list
+ * 
+ * Notes:
+ * - Traverses the list and frees each node one by one.
+ * - Ensures no memory leaks by freeing all dynamically allocated nodes.
+ */
 void freeList(ListNode* head) {
     ListNode* temp; 
     while (head != NULL) {
@@ -84,6 +156,19 @@ void freeList(ListNode* head) {
     }
 }
 
+/*
+ * Function: main
+ * --------------
+ * Main function to drive the linked list program.
+ * 
+ * Steps:
+ * - Initializes an empty linked list (head = NULL).
+ * - Continuously prompts the user for input using nodeInput.
+ * - Adds each valid input to the linked list using addNode.
+ * - Breaks the loop if the user types "exit".
+ * - Prints the entire linked list using printList.
+ * - Frees the allocated memory using freeList before exiting.
+ */
 int main() {
     ListNode* head = NULL;
 
