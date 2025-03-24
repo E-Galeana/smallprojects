74004c0093eb7373af84bb5ae9a60a0ce0484416
E-Galeana <emanuelgaleana147@gmail.com>
2025-03-24 11:23:53 -0400

    Update include paths for raylib and implement user movement controls

--- game/src/main.c
+++ game/src/main.c
@@ -1,16 +1,38 @@
 #include "raylib.h"
 
+void userMoves(int *keyX, int *keyY) {
+    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
+        *keyX+= 3;
+    }
+    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
+        *keyX-= 3;
+    }
+    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) {
+        *keyY-= 3;
+    }
+    if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) {
+        *keyY+= 3;
+    }
+}
+
 int main() {
-    InitWindow(800, 600, "My Window");
+
+    int circleX = 400, circleY = 400;
+    
+    InitWindow(800, 800, "My Window");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+
+        // Event Handling
+        userMoves(&circleX, &circleY);
+        // Updating Positions
+
+        // Draw to screen
         BeginDrawing();
         { 
             ClearBackground(WHITE);
-
-            DrawText("Hello World", 10, 10, 10, BLACK);
-
+            DrawCircle(circleX, circleY, 50, BLACK);
         }
         EndDrawing();        
     }
